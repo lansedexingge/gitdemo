1288c877439a0e2b98dd7bf11e61b095921741aa
lansedexingge <1429976249@qq.com>
2019-07-14 18:34:46 +0800

    init

---
+++ gitfirst/src/main/TestMasterB.java
@@ -0,0 +1,11 @@
+package main;
+
+/**
+ * @Author zhouweibo
+ */
+public class TestMasterB {
+    
+    public static void main(String[] args){
+        System.out.println("test master b");
+    }
+}
